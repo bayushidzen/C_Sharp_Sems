874767d37f4f9a6d45337bc1e6fe8fe1df3cffaa
BayushiDzen <bayushidzen@yandex.ru>
2022-11-27 08:14:44 +0300

    HW for seminar 2 + special

---
+++ Task2.3/Task2.3.cs
@@ -0,0 +1,29 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите индекс дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch (a)
+{
+   case 1: Console.WriteLine("Понедельник - это не выходной");
+   break;
+   case 2: Console.WriteLine("Вторник - это не выходной");
+   break;
+   case 3: Console.WriteLine("Среда - это не выходной");
+   break;
+   case 4: Console.WriteLine("Четверг - это не выходной");
+   break;
+   case 5: Console.WriteLine("Пятница - это не выходной");
+   break;
+   case 6: Console.WriteLine("Суббота - ура! Отдыхаем!");
+   break;
+   case 7: Console.WriteLine("Воскресенье - продолжаем отдыхать!");
+   break;
+   default:Console.WriteLine("Вам надо отдохнуть!");
+   break;
+}
---
+++ Task2_Sp2/Task2_Sp2.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите количество программистов: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i=1;
+while (i<a)
+{
+    if (i < 100) 
+    {
+        if ((i > 10) && (i < 20)) Console.Write($"{i} программистов ");
+        else if ((i % 10 == 2)||(i % 10 == 3)||(i % 10 == 4)) Console.Write($"{i} программиста ");
+        else if (i % 10 == 1) Console.Write($"{i} программист ");
+        else Console.Write($"{i} программистов ");
+    }
+    else
+    {   
+        if ((i > 110) && (i < 120)) Console.Write($"{i} программистов ");
+        else if ((i % 10 == 2)||(i % 10 == 3)||(i % 10 == 4)) Console.Write($"{i} программиста ");
+        else if (i % 10 == 1) Console.Write($"{i} программист ");
+        else Console.Write($"{i} программистов ");
+    }
+    i++;
+}
